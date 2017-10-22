8b62f30a002e434c5bd9440a99251021f3c404b6
Seethalakshmi Ramaiah <sr922@afsconnect2.njit.edu>
2017-10-21 23:16:13 -0400

    All classes in one file

--- index.php
+++ index.php
@@ -1,81 +1,263 @@
-<!DOCTYPE HTML>  
-<html>
-<head>
-<style>
-.error {color: #FF0000;}
-</style>
-</head>
-<body>  
-
 <?php
-// define variables and set to empty values
-$nameErr = $emailErr = $genderErr = $websiteErr = "";
-$name = $email = $gender = $comment = $website = "";
 
-if ( isset($_POST["submit"]) ) {
+//turn on debugging messages
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
 
-   if ( isset($_FILES["file"])) {
 
-        //if there was an error uploading the file
-        if ($_FILES["file"]["error"] > 0) {
-        	echo "Return Code: " . $_FILES["file"]["error"] . "<br />";
+//instantiate the program object
 
-        }
-        else {
-            //Print file details
-            echo "Upload: " . $_FILES["file"]["name"] . "<br />";
-            echo "Type: " . $_FILES["file"]["type"] . "<br />";
-            echo "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br />";
-            echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br />";
-
-                 //if file already exists
-            if (file_exists("upload/" . $_FILES["file"]["name"])) {
-            	echo $_FILES["file"]["name"] . " already exists. ";
-            }
-            else {
-            	//Store file in directory "upload" with the name of "uploaded_file.txt"
-	            $storagename = "uploaded_file.txt";
-	            move_uploaded_file($_FILES["file"]["tmp_name"], "upload/" . $storagename);
-	            echo "Stored in: " . "upload/" . $_FILES["file"]["name"] . "<br />";
-            }
-        }
-     } else {
-        echo "No file selected <br />";
-     }
+//Class to load classes it finds the file when the progrm starts to fail for calling a missing class
+class Manage {
+    public static function autoload($class) {
+        //you can put any file name or directory here
+        include $class . '.php';
+    }
 }
 
-function test_input($data) {
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  return $data;
+spl_autoload_register(array('Manage', 'autoload'));
+
+//instantiate the program object
+$obj = new main();
+
+
+class main {
+
+    public function __construct()
+    {
+        //print_r($_REQUEST);
+        //set default page request when no parameters are in URL
+        $pageRequest = 'uploadform';
+        //check if there are parameters
+        if(isset($_REQUEST['page'])) {
+        	//load the type of page the request wants into page request
+            $pageRequest = $_REQUEST['page'];
+        }
+        //instantiate the class that is being requested
+        $page = new $pageRequest;
+
+
+        if($_SERVER['REQUEST_METHOD'] == 'GET') {
+        	$page->get();
+        } else {
+        	echo "coming here";
+            $page->post();
+        }
+
+    }
+
 }
 
+abstract class page {
+    protected $html;
+
+    public function __construct()
+    {
+        $this->html .= '<html>';
+        $this->html .= '<link rel="stylesheet" href="styles.css">';
+        $this->html .= '<body>';
+    }
+    public function __destruct()
+    {
+        $this->html .= '</body></html>';
+        // stringFunctions::printThis($this->html);
+    }
+
+    public function get() {
+        echo 'default get message';
+    }
+
+    public function post() {
+        print_r($_POST);
+    }
+}
+
+class homepage extends page {
+
+    public function get() {
+
+    	$form = '<form action="index2.php" method="post">';
+        $form .= 'First name:<br>';
+        $form .= '<input type="text" name="firstname" value="Mickey">';
+        $form .= '<br>';
+        $form .= 'Last name:<br>';
+        $form .= '<input type="text" name="lastname" value="Mouse">';
+        $form .= '<input type="submit" value="Submit">';
+        $form .= '</form> ';
+        $this->html .= 'homepage';
+        $this->html .= $form;
+
+        echo $form;
+    }
+
+}
+
+class uploadform extends page
+{
+
+    public function get()
+    {
+    	$form  = "<h2>Parsing CSV Files</h2>";
+		$form .= "<p><span class=\"error\">* required field.</span></p>";
+        $form .= '<form action="index.php?page=uploadform" method="post"
+	enctype="multipart/form-data">';
+		$form .= "Select CSV file to upload: *<br/><br/>";
+        $form .= '<input type="file" name="file" id="file"><br/><br/>';
+        $form .= '<input type="submit" value="Upload CSV" name="submit">';
+        $form .= '</form> ';
+        $this->html .= $form;
+
+        echo $form;
+
+    }
+
+    public function post() {
+        $file = new file;
+        $url = 'https://web.njit.edu/~sr922/Project-1/index.php?page=parsedCSV&';
+
+        if ( isset($_FILES["file"])) {
+
+	        //if there was an error uploading the file
+	        if ($_FILES["file"]["error"] > 0) {
+	        	echo "Return Code: " . $_FILES["file"]["error"] . "<br />";
+
+	        }
+	        else {
+	            $file->set_name($_FILES["file"]["name"]);
+	            $file->set_type($_FILES["file"]["type"]);
+	            $file->set_size($_FILES["file"]["size"]);
+	            $file->set_tmp_name($_FILES["file"]["tmp_name"]);
+	            
+	            echo "Upload: " . $file->get_name() . "<br />";
+	            echo "Type: " . $file->get_type() . "<br />";
+	            echo "Size: " . $file->get_size() . " Kb<br />";
+	            echo "Temp file: " . $file->get_tmp_name() . "<br />";
+
+	            //if file already exists
+	            if (file_exists("upload/" . $_FILES["file"]["name"])) {
+	            	echo $_FILES["file"]["name"] . " already exists. ";
+	            }
+	            else {
+	            	if (move_uploaded_file($_FILES['file']['tmp_name'], __DIR__ . '/uploads/' . $_FILES['file']['name'])) {
+					    echo "Uploaded";
+					} else {
+					   echo "File was not uploaded";
+					}
+					header('Location: '.$url."fileName=". __DIR__ . '/uploads/' . $_FILES['file']['name']);
+		            
+	            }
+	        }
+	     } else {
+	        echo "No file selected <br />";
+	    }
+    }
+}
 
 class File { 
-    public $path = ''; 
+    var $name;
+	
+    /* Setter Functions */
+	function set_name($name) {
+		$this->name = $name;
+	}
+
+	function set_type($type){
+		$this->type = $type;
+	}
+
+	function set_size($size) {
+		$this->size = $size / 1024;
+	}
+
+	function set_tmp_name($tmp_name){
+		$this->tmp_name = $tmp_name;
+	}
+
+	/* Getter Functions */
+	function get_name() {
+		return $this->name;
+	}
+
+	function get_type() {
+		return $this->type;
+	}
+
+	function get_size() {
+		return $this->size;
+	}
+
+	function get_tmp_name() {
+		return $this->tmp_name;
+	}
+
     
     
     function saveFile() { 
         print 'Inside `saveFile()`'; 
     } 
+}
 
-    function parseFile() {
 
+class parsedCSV extends page {
+	public function get()
+	{
+    	$fileName = $_GET["fileName"];
+    	Parser::parseCSV($fileName);
+
+	}
+
+	public function post(){
+
+	}
+}
+
+class Parser {
+    public static function parseCSV($fileName) {
+    	if ( $file = fopen($fileName , 'r' ) ) {
+
+    		echo "<h2>Parsed CSV File</h2>";
+
+		    $headers = fgets ($file, 4096 );
+		    
+		    $num_of_fields = strlen($headers) - strlen(str_replace(",", "", $headers));
+		    
+
+		    $fields = array();
+		    $fields = explode( ",", $headers, ($num_of_fields+1) );
+
+		    $line = array();
+		    $i = 0;
+
+		    while ( $line[$i] = fgets ($file, 4096) ) {
+
+		        $data[$i] = array();
+		        $data[$i] = explode( ",", $line[$i], ($num_of_fields+1) );
+
+		        $i++;
+		    }
+
+		        echo "<table>";
+		        echo "<tr>";
+		    for ( $k = 0; $k != ($num_of_fields+1); $k++ ) {
+		    	$fields[$k] = str_replace("\"", "", $fields[$k]);
+		        echo "<td>" . $fields[$k] . "</td>";
+		    }
+		        echo "</tr>";
+
+		    foreach ($data as $key => $number) {
+	    		echo "<tr>";
+		        foreach ($number as $k => $content) {
+		        	$content = str_replace("\"", "", $content);
+
+		        	echo "<td style=\"border-width: thin; border-style: solid;\">" . $content . "</td>";
+		        }
+		        echo "</tr>";
+		    	
+		    }
+
+		    echo "</table>";
+		}
     }
-} 
+}
 
-$file = new file; 
 ?>
-
-<h2>Parsing CSV Files</h2>
-<p><span class="error">* required field.</span></p>
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" enctype="multipart/form-data">  
-	Select CSV file to upload: *<br/><br/>
-    <input type="file" name="file" id="file"><br/><br/>
-    <input type="submit" value="Upload CSV" name="submit">
-  
-</form>
-
-</body>
-</html>
